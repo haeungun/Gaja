2acbbd637f7d8096bf664eb37fc5eab18715a93f
Haeun Kim <haeungun@gmail.com>
2017-06-22 18:39:45 +0900

    User info.

--- src/app/components/my-info/my-info.component.ts
+++ src/app/components/my-info/my-info.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-my-info',
   templateUrl: './my-info.component.html',
@@ -8,13 +10,37 @@ import { Router } from '@angular/router';
 })
 export class MyInfoComponent implements OnInit {
 
+  myInfo;
 
-  constructor(private router: Router,) { }
+  constructor(private router: Router,
+              private auth: AuthService) { 
+                let uid = this.auth.getCurrentUid();
+                console.log(uid);
+                this.auth.getUserInfoByUid(uid).subscribe(info => {
+                  console.log(info);
+                  this.myInfo = info;
+                });
+              }
 
   ngOnInit() {
   }
 
   showComponent(component) {
+    this.isStore();
+    if (component === "setting") {
+      if (!this.isStore()) {
+        alert("서비스 준비 중 입니다.");
+      }
+    }
     this.router.navigateByUrl('home/' + component);
+  }
+
+  isStore() {
+    let rule = this.auth.userRule();
+    if (rule === "Store") {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
